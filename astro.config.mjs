21f3a78ed439224b9e45ec085248c717833a482b
Riceball <42161586+Anderice@users.noreply.github.com>
2023-02-10 04:45:08 +0800

    refactor: Add base prefix to urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
+// Load env in astro config: https://docs.astro.build/en/guides/configuring-astro/#environment-variables
+import { loadEnv } from "vite";
+const { PUBLIC_URLBASE } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://riceball-tw.github.io",
-  base: "/hex-bootstrap",
+  base: PUBLIC_URLBASE,
 });
